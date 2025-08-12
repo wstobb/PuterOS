1cf2562bbf9b6110216a47d4fd391e0a11e29301
wstobb <ws@ivmm.net>
2025-08-12 12:35:16 -0400

    Fixed removed packages

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 # Uninstall
-dnf5 -y remove toolbox gnome-disk-utility gnome-tour
+dnf5 -y remove toolbox plasma-discover ark plasma-welcome kde-partitionmanager kwallet filelight kfind kwrite krfb
 
 # Install
 dnf5 -y install distrobox keepassxc obs-studio fastfetch code tailscale tmux zsh ghostty vim htop
