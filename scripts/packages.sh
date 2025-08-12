373a61a07dac8744c9442cd3704d3c4872b2759c
wstobb <ws@ivmm.net>
2025-08-12 16:40:53 -0400

    Removed Discord

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 # Uninstall
-dnf5 -y remove toolbox plasma-discover ark plasma-welcome kde-partitionmanager kwallet filelight kfind kwrite krfb
+dnf5 -y remove toolbox plasma-discover ark plasma-welcome kde-partitionmanager filelight kfind kwrite krfb
 
 # Install
-dnf5 -y install distrobox keepassxc obs-studio fastfetch code tailscale tmux zsh vim htop vlc discord
+dnf5 -y install distrobox keepassxc obs-studio fastfetch code tailscale tmux zsh vim htop vlc
--- scripts/repos.sh
+++ scripts/repos.sh
@@ -2,5 +2,4 @@
 
 set -ouex pipefail
 
-# Discord
-dnf5 -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# Nothing yet
