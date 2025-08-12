0bc765c5b92cf474125277ba7215a8519036ec93
wstobb <ws@ivmm.net>
2025-08-12 14:28:44 -0400

    Testing action 8

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 cp -rv /ctx/os_files/* /
-/ctx/build_files/copr.sh
-/ctx/build_files/packages.sh
+/ctx/scripts/copr.sh
+/ctx/scripts/packages.sh
 
 systemctl enable podman.socket
