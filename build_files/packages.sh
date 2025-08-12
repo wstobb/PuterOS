09d3b0f68b5c260e753710354e739524abe91093
wstobb <ws@ivmm.net>
2025-08-12 11:16:08 -0400

    Nevermind

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 # Uninstall
-dnf5 -y remove toolbox gnome-terminal gnome-disk-utility gnome-tour
+dnf5 -y remove toolbox gnome-disk-utility gnome-tour
 
 # Install
-dnf5 -y install distrobox flatpak keepassxc obs-studio fastfetch code tailscale tmux zsh nautilus-gsconnect gnome-tweaks gnome-shell-extension-gsconnect gnome-shell-extension-blur-my-shell gnome-shell-extension-caffeine gnome-shell-extension-gsconnect gnome-shell-extension-tailscale-gnome-qs ghostty
+dnf5 -y install distrobox flatpak keepassxc obs-studio fastfetch code tailscale tmux zsh nautilus-gsconnect gnome-tweaks gnome-shell-extension-gsconnect gnome-shell-extension-blur-my-shell gnome-shell-extension-caffeine gnome-shell-extension-gsconnect ghostty
