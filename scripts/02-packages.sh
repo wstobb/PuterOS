bc0d9f0b3700137b6a10d71e2515e72b0a69c10e
wstobb <ws@ivmm.net>
2025-08-13 19:36:10 -0400

    Reorganized

--- scripts/02-packages.sh
+++ scripts/02-packages.sh
@@ -2,14 +2,14 @@
 
 set -ouex pipefail
 
-if [ ! -s "/ctx/config/install_packages" ]; then
+if [ ! -s "/ctx/config/install_packages.list" ]; then
 	echo "No packages to install"
 else
-	dnf install -y $(tr '\n' ' ' < /ctx/config/install_packages)
+	dnf install -y $(tr '\n' ' ' < /ctx/config/install_packages.list)
 fi
 
-if [ ! -s "/ctx/config/remove_packages" ]; then
+if [ ! -s "/ctx/config/remove_packages.list" ]; then
 	echo "No packages to remove"
 else
-	dnf remove -y $(tr '\n' ' ' < /ctx/config/remove_packages)
+	dnf remove -y $(tr '\n' ' ' < /ctx/config/remove_packages.list)
 fi
--- scripts/03-services.sh
+++ scripts/03-services.sh
@@ -2,15 +2,15 @@
 
 set -ouex pipefail
 
-if [ ! -s "/ctx/config/enable_services" ]; then
+if [ ! -s "/ctx/config/enable_services.list" ]; then
 	echo "No services to enable"
 else
-	systemctl enable $(tr '\n' ' ' < /ctx/config/enable_services)
+	systemctl enable $(tr '\n' ' ' < /ctx/config/enable_services.list)
 fi
 
 if [ ! -s "/ctx/config/disable_services" ]; then
 	echo "No services to disable"
 else
-	systemctl disable $(tr '\n' ' ' < /ctx/config/disable_services)
+	systemctl disable $(tr '\n' ' ' < /ctx/config/disable_services.list)
 fi
 
